83d5d79661c45a35ef52a84be2f8fcc7f092d2f0
Gabi Guedes <zooiv3rde@gmail.com>
2023-04-21 23:44:06 -0300

    criando component de comentarios

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/gabiguedes.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gabriela Guedes</strong>
+                            <time title="11 de Abril às 22:03h" dataTime="2023-05-11 22:00">Cerca de 2h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns !! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}

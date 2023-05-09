9dc5a2a95dd1997e47e184bd613134ced6aa937b
Gabi Guedes <zooiv3rde@gmail.com>
2023-05-08 22:36:36 -0300

    validando comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,7 +34,12 @@ export function Post(props) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!')
     }
 
     function deleteComment(commentToDelete) {
@@ -79,9 +84,11 @@ export function Post(props) {
                     placeholder='Deixe um comentário'
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
                 <footer>
-                    <button type='submit'>Comentar</button>
+                    <button type='submit' disabled={newCommentText.length === 0}>Comentar</button>
                 </footer>
             </form>
 
